5654750bb7443c242c37b0bb08697d4d6c0df166
Sarawut Ponsan <phaiza237@gmail.com>
2025-08-03 00:07:32 +0700

    Update Dijkstra.h

--- css121/self-practice/Graph/Dijkstra.h
+++ css121/self-practice/Graph/Dijkstra.h
@@ -29,6 +29,7 @@ std::vector<int> dijkstra(std::vector<std::vector<int>> &edges, int vertex, int 
         {
             int v = a.first;
             int w = a.second;
+            
             if (dist[v] > dist[u] + w)
             {
                 dist[v] = dist[u] + w;
