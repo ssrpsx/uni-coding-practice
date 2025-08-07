f61b38e76c44ce2559aac6c2732e15c41b4644b5
Sarawut Ponsan <phaiza237@gmail.com>
2025-08-07 22:49:37 +0700

    Self-learing: namespace c++

---
+++ css121/self-practice/Basic/namespace.h
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using std::cout;
+
+namespace first_space {
+    void func() 
+    {
+        cout << "Inside first_space" << std::endl;
+    }
+}
+
+// first_space::func();
+
+/* 
+namespace outer {
+    void fun(){
+        cout << "Inside outer namespace" << endl;
+    }
+    
+    // Inner namespace
+    namespace inner {
+        void func() {
+            cout << "Inside inner namespace";
+        }
+    }
+}
+*/
+
+// outer::inner::func(); 
